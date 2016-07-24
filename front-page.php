61c2a97f53ee8f9a914f5ce68eddcb719897d162
Trevor MW <tmwagner66@gmail.com>
2016-07-24 13:47:58 -0400

    Removing AMD/require code from aja-form loader to test

--- front-page.php
+++ front-page.php
@@ -9,8 +9,8 @@ get_header(); the_post(); ?>
 
 <div data-load-async="load_home_form" data-load-on="async:load"></div>
 
-<form data-ajax-form data-action="load_home_form">
-  <input type="text" name="" value="" />
+<form data-ajax-form data-action="load_home_form" data-confirm="true">
+  <input type="text" name="hello" value="" />
   <button type="submit">Help</button>
 </form>
 
