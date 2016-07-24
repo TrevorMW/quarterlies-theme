25b5c1f7bd6cbd15a6ba37f407207edf7f131741
Trevor MW <tmwagner66@gmail.com>
2016-07-24 13:57:14 -0400

    Refactor core-ajax-form.js

--- assets/js/core/core-ajax-form.js
+++ assets/js/core/core-ajax-form.js
@@ -1,86 +1,99 @@
 /**
- * @package     Blueacorn/AjaxForm
+ * @package     AjaxForm
  * @version     1.0
- * @author      Blue Acorn <code@blueacorn.com>
- * @copyright   Copyright © 2016 Blue Acorn.
+ * @author     Trevor Wagner
  */
 
 ;(function ($, window, undefined ) {
 
-  var AjaxForm = {
-    form:{
-      el: null,
-      action: null,
-      confirm: false,
-      submit: null,
-      url:null,
-    },
-    data:{
-      formData: null
-    },
-    flags:{
-      canSubmit:false
-    },
-    init: function( form, url ) {
-      if (form.length > 0) {
-        this.form.el     = form;
-        this.form.submit = form.find('button[type="submit"]');
-        this.form.action = form.data('action');
-        this.form.url    = url;
+  var AjaxForm = function( ajax_url ){
 
-        if( form.data('confirm') !== undefined ){
-          this.form.confirm = form.data('confirm');
-        }
+    var form :{
+          el: null, action: null, confirm: false,submit: null, url:null,
+        },
+        data:{
+          formData: null
+        },
+        flags:{
+          canSubmit:false
+      };
+  };
+
+  AjaxForm.prototype.init = function( form, url )
+  {
+    if( form.length > 0 )
+    {
+      this.form.el     = form;
+      this.form.submit = form.find('button[type="submit"]');
+      this.form.action = form.data('action');
+      this.form.url    = url;
+
+      if( form.data('confirm') !== undefined )
+      {
+        this.form.confirm = form.data('confirm');
       }
+    }
 
-      this.collectData();
+    this.collectData();
 
-      if( this.confirmFormRequest() ){
-        this.makeRequest( this );
-      } else {
-        $(document).trigger('loader:hide');
-      }
-    },
-    setObservers: function( ajax_url ) {
-      $(document).on('submit', '[data-ajax-form]', function(e) {
-        e.preventDefault();
-        var form    = $(this),
-          formMsg = form.find('[data-form-msg]');
-
-        $(document).trigger( 'formMsg:init', formMsg );
-        AjaxForm.init( $(this), ajax_url );
-      });
-    },
-    collectData: function() {
-      this.data.formData = this.form.el.serialize();
-    },
-    confirmFormRequest: function() {
-      return this.form.confirm !== false ? confirm( this.form.confirm ) : true ;
-    },
-    makeRequest: function( inst ) {
-      $(document).trigger('loader:show').trigger('formMsg:clear');
-
-      // Ajax POST call using native DW library.
-      $.ajax({
-        method:'POST',
-        action:this.form.action,
-        url:this.form.url,
-        data:this.data.formData,
-        success:this.formSuccess
-      });
-    },
-    formSuccess:function( resp ){
+    if( this.confirmFormRequest() ){
+      this.makeRequest( this );
+    } else {
       $(document).trigger('loader:hide');
-
-      if( typeof resp == 'object' ){
-        $(document).trigger('formMsg:show', resp );
-      }
     }
   };
 
+  AjaxForm.prototype.setObservers = function( ajax_url )
+  {
+    $(document).on( 'submit', '[data-ajax-form]', function(e)
+    {
+      e.preventDefault();
+
+      var form    = $(this),
+          formMsg = form.find('[data-form-msg]');
+
+      $(document).trigger( 'formMsg:init', formMsg );
+      AjaxForm.init( $(this), ajax_url );
+    });
+  };
+
+  AjaxForm.prototype.collectData = function()
+  {
+    this.data.formData = this.form.el.serialize();
+  };
+
+  AjaxForm.prototype.confirmFormRequest = function()
+  {
+    return this.form.confirm !== false ? confirm( this.form.confirm ) : true ;
+  };
+
+  AjaxForm.prototype.makeRequest = function()
+  {
+    $(document).trigger('loader:show').trigger('formMsg:clear');
+
+    // Ajax POST call using native DW library.
+    $.ajax({
+      method:'POST',
+      action:this.form.action,
+      url:this.form.url,
+      data:this.data.formData,
+      success:this.formSuccess
+    });
+  };
+
+  AjaxForm.prototype.formSuccess = function()
+  {
+    $(document).trigger('loader:hide');
+
+    if( typeof resp == 'object' ){
+      $(document).trigger('formMsg:show', resp );
+    }
+  };
+
+
   $(document).on( 'core:load core:asyncLoad', function( e ) {
-    console.log( e, wpAjax );
-    AjaxForm.setObservers( wpAjax );
+    var ajaxForm = new AjaxForm( wpAjax );
+    ajaxForm.setObservers();
   });
 
 })(jQuery, window );
