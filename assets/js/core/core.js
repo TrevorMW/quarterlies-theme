f7a618b36104dd0d8b5a68e3e8d2520c97d2b06c
Trevor MW <tmwagner66@gmail.com>
2016-07-24 13:35:09 -0400

    Removing AMD/require code from aja-form loader to test

--- assets/js/core/core.js
+++ assets/js/core/core.js
@@ -2,11 +2,19 @@
 {
   // DEFINED GLOBAL EVENTS THAT CAN BE HOOKED INTO THROUGHOUT ALL LOADED JS.
   $(document).ready(function( e ){
-    $(document).trigger('core:load', e ).trigger('core:asyncLoad', e );
+    $(document).trigger('core:load', e).delay(500).trigger('core:asyncLoad', e);
   });
 
   $(window).resize(function( e ){
-    $(document).trigger('core:resize', e );
+    $(document).trigger('core:resize', e);
   });
+
+  $(document).on( 'core:load', function(){
+    alert('core loaded');
+  })
+
+  $(document).on( 'core:asyncLoad', function(){
+    alert('core loaded');
+  })
 
 })( jQuery, window );
